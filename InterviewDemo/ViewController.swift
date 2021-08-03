7486ff023b1189e4072706968ac3bff020bdd40b
ChenBingYuan <smallg1009@gmail.com>
2021-08-03 19:54:29 +0800

    test2

--- InterviewDemo/ViewController.swift
+++ InterviewDemo/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     var test: String?
+    
 
     @IBOutlet var redNumberButton: [UIButton]!
     @IBOutlet var blueNumberButton: [UIButton]!
